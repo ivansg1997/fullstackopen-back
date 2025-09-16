4e8ec5c4f5eabf7c8686aa1a2fd4f88da4bb5223
Iván Sánchez <139090781+ivansg1997@users.noreply.github.com>
2025-09-15 20:52:07 -0600

    Exercises 3.1 - 3.6

---
+++ part3/index.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+let persons = [
+    { 
+        "id": 1,
+        "name": "Arto Hellas", 
+        "number": "040-123456"
+    },
+    { 
+        "id": 2,
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
+    },
+    { 
+        "id": 3,
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
+    },
+    { 
+        "id": 4,
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
+    }
+]
+
+const date = new Date;
+const info = `
+        <div>
+            <p>Phonebook has info ${persons.length} people</p>
+            <p>${date.toUTCString()}</p>
+        </div>
+    `
+
+const generateId = () => {
+    return Math.floor(Math.random() * 100000)
+}
+
+app.get('/', (request, response) => {
+    response.send('<h1>Alo policia!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    response.send(info)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.statusMessage = "Not found person";
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const nameExist = persons.find(person => person.name === body.name); 
+
+    if (!body.name) {
+        return response.status(400).json({ 
+        error: 'name missing' 
+        })
+    }else if(!body.number) {
+        return response.status(400).json({ 
+        error: 'phone number missing' 
+        })
+    }else if(nameExist) {
+        return response.status(409).json({ 
+        error: 'user already exists' 
+        })
+    }
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
