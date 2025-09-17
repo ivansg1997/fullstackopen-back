cae7f10505a36d465dc862a0f634c69dbf3e4e17
Iván Sánchez <139090781+ivansg1997@users.noreply.github.com>
2025-09-17 14:39:42 -0600

    Exercises 3.19 - 3.21

--- part3/index.js
+++ part3/index.js
@@ -54,27 +54,19 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 });
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (!body.name) {
-        return response.status(400).json({ 
-        error: 'name missing' 
-        })
-    }else if(!body.number) {
-        return response.status(400).json({ 
-        error: 'phone number missing' 
-        })
-    }
 
     const person = new Person({
         name: body.name,
         number: body.number,
     });
 
-    person.save().then(savePerson => {
-        response.json(savePerson)
-    })
+    person.save()
+        .then(savePerson => {
+            response.json(savePerson)
+        })
+        .catch(error => next(error))
 });
 
 const unknownEndpoint = (request, response) => {
@@ -88,7 +80,9 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformatted id' })
-    } 
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    }
 
     next(error)
 };
--- part3/models/note.js
+++ part3/models/note.js
@@ -14,8 +14,21 @@ mongoose.connect(url)
     })
 
 const phonebookSchema = new mongoose.Schema({
-    name: String,
-    number: Number,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        required: true,
+        validate: {
+            validator: function(v) {
+                return /^\d{2,3}-\d{8,}$/.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+    },
 })
 
 phonebookSchema.set('toJSON', {
